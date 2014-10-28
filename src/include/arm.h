723b699d32bc4e11a13e9c26751692ee3227946e
Date Huang <tjjh89017@hotmail.com>
2014-10-28 15:35:00 +0800

    add Interrupt mode and SYS mode

--- src/include/arm.h
+++ src/include/arm.h
@@ -6,4 +6,18 @@
 #endif
 
 
+// cpsr|spsr intrrupt mode
+#define NO_INT      0xc0
+#define DIS_INT     0x80
+
+// ARM 7 mode
+#define MODE_MASK   0x1f
+#define USR_MODE    0x10
+#define FIQ_MODE    0x11
+#define IRQ_MODE    0x12
+#define SVC_MODE    0x13
+#define ABT_MODE    0x17
+#define UND_MODE    0x1b
+#define SYS_MODE    0x1f
+
 #endif
