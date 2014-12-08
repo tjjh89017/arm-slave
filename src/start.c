b31e8a3b1d204b1cff6e39c1b02431e18bce1b59
Date Huang <tjjh89017@hotmail.com>
2014-12-08 17:02:14 +0800

    base start for output booting to screen

--- src/start.c
+++ src/start.c
@@ -1,9 +1,19 @@
 #include "arm.h"
 #include "type.h"
 
+/* TODO need to find a place to put it */
+#define UART0 0x101f1000
 void _uart_putc(int c)
 {
-	volatile
+	volatile uint8 *uart0 = (uint8*)UART0;
+	*uart0 = c;
+}
+
+void _puts(char *s)
+{
+	for(; *s != '\0'; s++){
+		_uart_putc(*s);
+	}
 }
 
 void start()
