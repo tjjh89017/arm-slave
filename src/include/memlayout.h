f10ce8862e6bef69727236e488ff651762d9f652
Date Huang <tjjh89017@hotmail.com>
2014-12-16 16:27:03 +0800

    set memlayout

---
+++ src/include/memlayout.h
@@ -0,0 +1,16 @@
+#ifndef __MEMLAYOUT_H__
+#define __MEMLAYOUT_H__
+
+#define EXTMEM 0x20000
+#define KERNEL_BASE 0xc0000000
+#define KERNEL_LINK (KERNEL_BASE + EXTMEM)
+
+#define INIt_KERNEL_MAP 0x100000
+
+#define virt_to_phys(a) (((uint)(a)) - KERNEL_BASE)
+#define phys_to_virt(a) (((void*)(a)) + KERNEL_BASE)
+
+#define virt_to_phys_wo(x) ((x) - KERNEL_BASE)
+#define phys_to_virt_wo(x) ((x) + KERNEL_BASE)
+
+#endif
