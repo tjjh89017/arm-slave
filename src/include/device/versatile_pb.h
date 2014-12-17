291c582e93bf8530fd90dda65b1e5ea544dd9236
Date Huang <tjjh89017@hotmail.com>
2014-12-17 10:12:31 +0800

    rename macro

--- src/include/device/versatile_pb.h
+++ src/include/device/versatile_pb.h
@@ -1,9 +1,9 @@
 #ifndef VERSATILE_PB
 #define VERSATILE_PB
 
-#define DEVBASE         0x10000000
-#define DEV_MEM_SZ      0x08000000
-#define VEC_TBL         0xffff0000
-#define UART0           0x101f1000
+#define DEVBASE           0x10000000
+#define DEV_MEM_SIZE      0x08000000
+#define VEC_TBL           0xffff0000
+#define UART0             0x101f1000
 
 #endif
