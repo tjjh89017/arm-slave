8a35c1f07c195d06b65363c74ece152132e670a4
Date Huang <tjjh89017@hotmail.com>
2014-12-17 10:13:58 +0800

    rename macro

--- src/include/device/versatile_pb.h
+++ src/include/device/versatile_pb.h
@@ -1,7 +1,7 @@
 #ifndef VERSATILE_PB
 #define VERSATILE_PB
 
-#define DEVBASE           0x10000000
+#define DEV_BASE           0x10000000
 #define DEV_MEM_SIZE      0x08000000
 #define VEC_TBL           0xffff0000
 #define UART0             0x101f1000
--- src/start.c
+++ src/start.c
@@ -37,6 +37,7 @@ void _putint(char *prefix, uint val, char * suffix)
 
 extern uint32 _kernel_pgd;
 extern uint32 _user_pgd;
+extern void *end;
 
 uint32 *kernel_pgd = &_kernel_pgd;
 uint32 *user_pgd = &_user_pgd;
@@ -82,12 +83,23 @@ void set_bootpgd(uint32 virt, uint32 phys, uint len, int dev_mem)
 
 void start()
 {
+	uint32 vectbl;
 	_puts("booting...\n");
 	
 	// set pgd
 	set_bootpgd(0, 0, INIT_KERNEL_MAP, 0);
 	set_bootpgd(KERNEL_BASE, 0, INIT_KERNEL_MAP, 0);
 
+	vectbl = phys_to_virt_wo(VEC_TBL & PGD_MASK);
+	if(vectbl <= (uint32)&end){
+		_puts("error: vector table overlaps kernel\n");
+	}
+
+	set_bootpgd(VEC_TBL, 0, 1 << PGD_SHIFT, 0);
+	set_bootpgd(KERNEL_BASE + DEVBASE, DEV_BASE, DEV_MEM_SIZE, 1);
+
+#ifdef __DEBUG__
+	
 	// before enable MMU phys address works
 	int i = 0;
 	for(i = 0; i < 0x4000 / 4; i++){
@@ -102,5 +114,5 @@ void start()
 			_putint(NULL, user_pgd[i], "\n");
 		}
 	}
-
+#endif
 }
