5c26bf13f64960d385ed6e460b719d0b6ed3bac6
Date Huang <tjjh89017@hotmail.com>
2015-03-26 19:39:11 +0800

    cpu state

---
+++ src/include/proc.h
@@ -0,0 +1,16 @@
+#ifndef __PROC_H__
+#define __PROC_H__
+
+#include "arm.h"
+#include "type.h"
+
+struct cpu {
+	uchar id;
+	
+	// depth of pushcli nesting
+	int ncli;
+	// were interrupts enabled before pushcli?
+	int interna;
+};
+
+#endif
