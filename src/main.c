3a09b155a8433b79708bd48084b2a0453be64d13
Date Huang <tjjh89017@hotmail.com>
2015-05-16 17:38:43 +0800

    fix

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@ void kmain()
 	init_vm();
 
 	UART0_puts("kernel_mem_freerange\n");
-	uint vector_table = phys_to_virt_wo(VECTOR_TABLE);
+	uint vector_table = phys_to_virt_wo(VECTOR_TABLE & PGD_MASK);
 	kernel_mem_freerange(align_up(&end, PTE_SIZE), vector_table);
 	kernel_mem_freerange(vector_table + PTE_SIZE, phys_to_virt_wo(INIT_KERNEL_MAP));
 
