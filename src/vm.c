24ada6daa68ef187b9c72886e3918f0cbe293d02
Date Huang <tjjh89017@hotmail.com>
2015-05-16 16:18:01 +0800

    init vm, let all mem can be alloc

---
+++ src/vm.c
@@ -0,0 +1,39 @@
+#include "arm.h"
+#include "mmu.h"
+#include "spinlock.h"
+
+// define at kernel.ld
+extern char data[];
+
+pde_t *kernel_pde;
+
+// mem alloc
+struct run {
+	struct run *next;
+};
+
+struct {
+	struct spinlock lock;
+	struct run *freelist;
+} kernel_mem;
+
+void init_vm()
+{
+	initlock(&kernel_mem.lock, "vm");
+	kernel_mem.freelist = NULL;
+}
+
+void _kernel_mem_free(char *v)
+{
+	struct run *r = (struct run*)v;
+	r->next = kernel_mem.freelist;
+	kernel_mem.freelist = r;
+}
+
+void kernel_mem_freerange(uint32 low, uint32 high)
+{
+	while(low < hi){
+		_kernel_mem_free((char*)low);
+		low += PTE_SIZE;
+	}
+}
